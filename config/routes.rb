9945a545e2612931b3c576bed406e7f2891b23ed
Olga Starkova <olstar.ru@gmail.com>
2017-02-06 18:11:50 +0100

    2nd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/game', to: 'sessions#game'
+
+  get '/score', to: 'sessions#score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
